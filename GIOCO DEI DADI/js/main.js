0a5983387e338965b37d7c96d0cdf24592b1a37f
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-01-26 17:55:50 +0100

    modify DOM with js

--- GIOCO DEI DADI/js/main.js
+++ GIOCO DEI DADI/js/main.js
@@ -5,20 +5,45 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 // SVOLGIMENTO
-// Genera numero randomico da 1 a 6 per PC
-const numberPc = Math.floor(Math.random() * 6) + 1;
-console.log("Il numero del PC è " + numberPc);
-// Genera numero randomico da 1 a 6 per utente
-const numberUser = Math.floor(Math.random() * 6) + 1;
-console.log("Il numero dell'utente è " + numberUser);
-// SE numero PC più alto vince
-if (numberPc > numberUser) {
-    console.log("Numero PC: " + numberPc + ", Numero utente: " + numberUser + "\n" + "Vince il PC.");
+// Creo costante bottone che darà il via all'evento
+const calculationButton = document.getElementById("calculation_button");
 
-    // ALTRIMENTI vince utente
-} else if (numberPc == numberUser) {
-    console.log("Numero PC: " + numberPc + ", Numero utente: " + numberUser + "\n" + "Pareggio.");
-} else {
-    console.log("Numero PC: " + numberPc + ", Numero utente: " + numberUser + "\n" + "Vince l'utente.");
-}
+calculationButton.addEventListener(
+    "click",
+    function () {
+        // Genera numero randomico da 1 a 6 per PC
+        const numberPc = Math.floor(Math.random() * 6) + 1;
+        // console.log("Il numero del PC è " + numberPc);
+        document.getElementById("number_pc").innerHTML = numberPc;
+
+        // Genera numero randomico da 1 a 6 per utente
+        const numberUser = Math.floor(Math.random() * 6) + 1;
+        // console.log("Il numero dell'utente è " + numberUser);
+        document.getElementById("number_user").innerHTML = numberUser;
+
+        // SE numero PC più alto vince
+        if (numberPc > numberUser) {
+            console.log("Numero PC: " + numberPc + ", Numero utente: " + numberUser + "\n" + "Vince il PC.");
+            let outputMessage = ("Mi dispiace, ha vinto il PC.");
+            document.getElementById("output_message").innerHTML = outputMessage;
+
+            // ALTRIMENTI SE numero uguale pareggiano
+        } else if (numberPc == numberUser) {
+            console.log("Numero PC: " + numberPc + ", Numero utente: " + numberUser + "\n" + "Pareggio.");
+            let outputMessage = ("Poteva andare peggio, è un pareggio.");
+            document.getElementById("output_message").innerHTML = outputMessage;
+
+            // ALTRIMENTI vince utente
+        } else {
+            console.log("Numero PC: " + numberPc + ", Numero utente: " + numberUser + "\n" + "Vince l'utente.");
+            let outputMessage = ("COMPLIMENTI, HAI VINTO!");
+            document.getElementById("output_message").innerHTML = outputMessage;
+
+        };
+
+
+
+    }
+);
+
 
