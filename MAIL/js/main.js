2e48b64317faef96e390dfcfe244c93f452b3960
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-01-26 14:33:56 +0100

    add mail checked

--- MAIL/js/main.js
+++ MAIL/js/main.js
@@ -6,14 +6,30 @@
 // stampa un messaggio appropriato sull'esito del controllo.
 
 // SVOLGIMENTO
+// chiedere email all'uente
+let userMail = prompt("Qual'è la tua email?");
+
 // creare un array di email con il quale confrontare
-const mailList = ["mail@mail.it", "mail@mail.com", "mymail@mymail.it", "mymail@mail.com"];
+const mailList = ["mail@mail.it", "mail@mail.com", "mymail@mymail.it", "mymail@mymail.com"];
 
 // creare variabile interruttore per verificare quando diventerà vera
+let mailChecked = false;
+
 // identificare tutte le mail dell'array
 for (let i = 0; i < mailList.length; i++) {
     console.log(mailList[i]);
+
+    // verificare che la mail sia presente nella lista dell'array
+    if (mailList[i] == userMail) {
+        mailChecked = true;
+    }
 }
-// chiedere email all'uente
-// verificare che la mail sia presente nella lista dell'array
+
 // stampare esito controllo
+if (mailChecked == true) {
+    console.log("Benvenuto " + userMail + ", puoi accedere.");
+
+} else {
+    alert("Non hai i permessi per accedere.")
+
+}
